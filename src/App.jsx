09458ea4418134d5bae02465c6d9efcf899bc9a2
WorkAccVikas <workaccvikas@gmail.com>
2023-05-27 18:03:37 +0530

    TMDB API Setup - API Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+
 function App() {
+  useEffect(() => {
+    console.log("In useEffect");
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <>
       <div className="App">App</div>
