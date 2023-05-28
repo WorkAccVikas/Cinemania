05e979ff572e694c236539d08c6e3ea747458dc2
WorkAccVikas <workaccvikas@gmail.com>
2023-05-29 01:26:32 +0530

    Homepage : Popular and Top Rated Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <Trending />
-      <div style={{ height: "1000px" }}>123</div>
+      <Popular />
+      <TopRated />
     </div>
   );
 };
