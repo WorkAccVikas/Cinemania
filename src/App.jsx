1dd3dc1c65d1430549e25505b973456246743c7b
WorkAccVikas <workaccvikas@gmail.com>
2023-05-28 12:53:35 +0530

    Homepage - Hero Banner Section CSS

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,24 @@ function App() {
 
   useEffect(() => {
     console.log("In useEffect");
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
-      console.log(res);
-      dispatch(getApiConfiguration(res));
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log("Result = ", res);
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
     });
   };
   return (
     <>
       <BrowserRouter>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:mediaType/:id" element={<Details />} />
@@ -41,7 +46,7 @@ function App() {
           <Route path="/search/:query" element={<SearchResult />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
